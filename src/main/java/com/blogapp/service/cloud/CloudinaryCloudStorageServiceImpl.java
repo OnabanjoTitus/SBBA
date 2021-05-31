1ddf7a264ca9e473b2cec9de52cafc7390e8d393
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-05-31 16:25:42 +0100

    springboot

---
+++ src/main/java/com/blogapp/service/cloud/CloudinaryCloudStorageServiceImpl.java
@@ -0,0 +1,21 @@
+package com.blogapp.service.cloud;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+
+@Service
+public class CloudinaryCloudStorageServiceImpl implements CloudStorageService {
+    @Autowired
+    Cloudinary cloudinary;
+    @Override
+    public Map<Object,Object> uploadImage(Map<Object, Object> imageProperties) throws IOException {
+        cloudinary.uploader().upload(imageProperties.get("file"), ObjectUtils.emptyMap());
+        return null;
+    }
+}
