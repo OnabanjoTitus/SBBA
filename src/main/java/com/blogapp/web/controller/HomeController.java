53d515327a66f71c358ac1d0ee8c875ae78f2693
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-06-11 17:00:40 +0100

    spring boot

--- src/main/java/com/blogapp/web/controller/HomeController.java
+++ src/main/java/com/blogapp/web/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.blogapp.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public  @ResponseBody String showWelcome(){
+    public  @ResponseBody String showWelcome(Model model){
         return "index";
     }
 
