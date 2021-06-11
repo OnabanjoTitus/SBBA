04b698911055aea3f41b4fcc29c97f5f235d338a
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-06-11 16:03:38 +0100

    spring boot

--- src/test/java/com/blogapp/BlogAppApplicationTests.java
+++ src/test/java/com/blogapp/BlogAppApplicationTests.java
@@ -1,35 +1,35 @@
-//package com.blogapp;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import javax.sql.DataSource;
-//
-//import java.sql.Connection;
-//import java.sql.SQLException;
-//
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//
-//@SpringBootTest
-//@Slf4j
-//class BlogAppApplicationTests {
-//	@Autowired
-//	DataSource dataSource;
-//
-//	@Test
-//	void applicationsCanConnectToDatabaseTest() throws SQLException {
-//		assertThat(dataSource).isNotNull();
-//		log.info("Datasource object is created");
-//
-//		try(Connection connection=dataSource.getConnection()){
-//			assertThat(connection).isNotNull();
-//			assertThat(connection.getCatalog()).isEqualTo("blogdb");
-//			log.info("Connection-->{}",connection.getCatalog());
-//		}catch (SQLException e){
-//			log.info("Exception occured while"+"connecting to database-->{}",e.getMessage());
-//		}
-//	}
-//
-//}
+package com.blogapp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@Slf4j
+class BlogAppApplicationTests {
+	@Autowired
+	DataSource dataSource;
+
+	@Test
+	void applicationsCanConnectToDatabaseTest() throws SQLException {
+		assertThat(dataSource).isNotNull();
+		log.info("Datasource object is created");
+
+		try(Connection connection=dataSource.getConnection()){
+			assertThat(connection).isNotNull();
+			assertThat(connection.getCatalog()).isEqualTo("blogdb");
+			log.info("Connection-->{}",connection.getCatalog());
+		}catch (SQLException e){
+			log.info("Exception occured while"+"connecting to database-->{}",e.getMessage());
+		}
+	}
+
+}
--- src/test/java/com/blogapp/service/PostServiceImplTest.java
+++ src/test/java/com/blogapp/service/PostServiceImplTest.java
@@ -1,52 +1,52 @@
-//package com.blogapp.service;
-//
-//import com.blogapp.data.models.Post;
-//import com.blogapp.data.repository.PostRepository;
-//import com.blogapp.web.dto.PostDto;
-//import com.blogapp.web.exceptions.PostObjectIsNullException;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import static org.mockito.Mockito.*;
-//import org.mockito.MockitoAnnotations;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class PostServiceImplTest {
-//
-//    @Mock
-//    PostRepository postRepository;
-//
-//    @InjectMocks
-//    PostService postServiceImpl = new PostServiceImpl();
-//    Post testPost;
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.openMocks(this);
-//       testPost= new Post();
-//    }
-//    private String extractFileName(String fileName){
-//        return fileName.split("\\.")[0];
-//    }
-//
-//
-//    @Test
-//    void whenTheSaveMethodIsCalled_thenRepositoryIsCalledOnceTest() throws PostObjectIsNullException {
-//        when(postServiceImpl.savePost(new PostDto())).thenReturn(testPost);
-//        postServiceImpl.savePost(new PostDto());
-//
-//        verify(postRepository,times(1)).save(testPost);
-//    }
-//    @Test
-//    void whenTheFindAllMethodIsCalled_thenReturnAListOfPost(){
-//        List<Post> postList= new ArrayList<>();
-//        when(postServiceImpl.findAllPost()).thenReturn(postList);
-//        postServiceImpl.findAllPost();
-//        verify(postRepository,times(1));
-//    }
-//}
+package com.blogapp.service;
+
+import com.blogapp.data.models.Post;
+import com.blogapp.data.repository.PostRepository;
+import com.blogapp.web.dto.PostDto;
+import com.blogapp.web.exceptions.PostObjectIsNullException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceImplTest {
+
+    @Mock
+    PostRepository postRepository;
+
+    @InjectMocks
+    PostService postServiceImpl = new PostServiceImpl();
+    Post testPost;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+       testPost= new Post();
+    }
+    private String extractFileName(String fileName){
+        return fileName.split("\\.")[0];
+    }
+
+
+    @Test
+    void whenTheSaveMethodIsCalled_thenRepositoryIsCalledOnceTest() throws PostObjectIsNullException {
+        when(postServiceImpl.savePost(new PostDto())).thenReturn(testPost);
+        postServiceImpl.savePost(new PostDto());
+
+        verify(postRepository,times(1)).save(testPost);
+    }
+    @Test
+    void whenTheFindAllMethodIsCalled_thenReturnAListOfPost(){
+        List<Post> postList= new ArrayList<>();
+        when(postServiceImpl.findAllPost()).thenReturn(postList);
+        postServiceImpl.findAllPost();
+        verify(postRepository,times(1));
+    }
+}
